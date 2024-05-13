36d9381428ce307c0d8646020bf60b43816021ae
rla5233 <rla5233@naver.com>
2024-05-13 17:48:48 +0900

    김수원 : VilageMap - Wall 생성

--- CrazyArcade/Contents/VillageMap.cpp
+++ CrazyArcade/Contents/VillageMap.cpp
@@ -16,8 +16,44 @@ void AVillageMap::BeginPlay()
 
 	SetBackGround(MapImgRes::village_background);
 	SetMapInfoSize(15, 13);
+	
+	// Wall »ý¼º
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			CreateWall(2 * i, 2 * j, MapImgRes::block_house_1);
+		}
+	}
 
-	CreateWall(0, 0, MapImgRes::block_house_1);
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			CreateWall(10 + 2 * i, 8 + 2 * j, MapImgRes::block_house_2);
+		}
+	}
+
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			CreateWall(1 + 2 * i, 7 + 2 * j, MapImgRes::block_house_0);
+		}
+	}
+
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			CreateWall(11 + 2 * i, 1 + 2 * j, MapImgRes::block_house_0);
+		}
+	}
+
+
+
+
+
 }
 
 void AVillageMap::Tick(float _DeltaTime)
