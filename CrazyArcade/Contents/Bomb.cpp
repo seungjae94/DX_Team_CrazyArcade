64fb88d3fd90358311bb490dc55f69bbe7e2b1d1
TaeHwan <alex23456@naver.com>
2024-05-14 15:30:03 +0900

    이태환: Bomb Class 추가

---
+++ CrazyArcade/Contents/Bomb.cpp
@@ -0,0 +1,26 @@
+#include "PreCompile.h"
+#include "Bomb.h"
+
+ABomb::ABomb() 
+{
+	DefaultComponent = CreateDefaultSubObject<UDefaultSceneComponent>("DefaultComponent");
+	SetRoot(DefaultComponent);
+
+	Renderer = CreateDefaultSubObject<USpriteRenderer>("Renderer");
+	Renderer->SetupAttachment(DefaultComponent);
+}
+
+ABomb::~ABomb() 
+{
+}
+
+void ABomb::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABomb::Tick(float _DeltaTime)
+{
+
+}
+
---
+++ CrazyArcade/Contents/Bomb.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+// Ό³Έν :
+class ABomb : public AActor
+{
+	GENERATED_BODY(AActor)
+
+public:
+	// constrcuter destructer
+	ABomb();
+	~ABomb();
+
+	// delete Function
+	ABomb(const ABomb& _Other) = delete;
+	ABomb(ABomb&& _Other) noexcept = delete;
+	ABomb& operator=(const ABomb& _Other) = delete;
+	ABomb& operator=(ABomb&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+private:
+	UDefaultSceneComponent* DefaultComponent;
+	USpriteRenderer* Renderer;
+};
+
