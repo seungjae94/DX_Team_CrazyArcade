91ad15f486bcd05886e6266e6dfae1d1213b8ad7
YHJ <yhjin07@naver.com>
2024-05-14 17:49:14 +0900

    진연호: MainPlayLevel Item Order 테스트용 Item 스폰

--- CrazyArcade/Contents/Item.cpp
+++ CrazyArcade/Contents/Item.cpp
@@ -7,16 +7,12 @@ AItem::AItem()
 	DefaultComponent = CreateDefaultSubObject<UDefaultSceneComponent>("DefaultComponent");
 	SetRoot(DefaultComponent);
 
-	Order = PlayLevel->GetMap()->GetRenderOrder(GetActorLocation());
-
 	Renderer = CreateDefaultSubObject<USpriteRenderer>("Renderer");
 	Renderer->SetupAttachment(DefaultComponent);
 	Renderer->AddPosition({ 0.0f, (BlockSize / 2.0f) });	
-	Renderer->SetOrder(Order);
 
 	ShadowRenderer = CreateDefaultSubObject<USpriteRenderer>("ShadowRenderer");
 	ShadowRenderer->SetupAttachment(DefaultComponent);
-	ShadowRenderer->SetOrder(Order - 1);
 }
 
 AItem::~AItem()
@@ -38,7 +34,9 @@ void AItem::Tick(float _DeltaTime)
 {
 	Super::Tick(_DeltaTime);
 
-	
+	Order = PlayLevel->GetMap()->GetRenderOrder(GetActorLocation());
+	Renderer->SetOrder(Order);
+	ShadowRenderer->SetOrder(Order - 1);
 
 	MoveUpDown(_DeltaTime);
 }
