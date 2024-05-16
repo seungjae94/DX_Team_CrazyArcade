a3f0b4896426ed003c49726831ba55cf5f4b8f14
박경택 <rudxorrmrwkd@gmail.com>
2024-05-16 18:24:24 +0900

    박경택: 인게임 UI 작업 결과창 작업

--- CrazyArcade/Contents/InGameUI.h
+++ CrazyArcade/Contents/InGameUI.h
@@ -23,12 +23,18 @@ private:
 	void CountDown(float _DeltaTime);
 	void TimerText();
 	void IntToText();
+
+
 	std::string MinText;
 	std::string SecText;
 
 	int Min = 0;
+	
+	
 	UImage* CancelBtn = nullptr;
-
+	UImage* ResultBackGround = nullptr;
+	UImage* ResultSummary = nullptr;
+	std::vector<UImage*> ScoreUI = {nullptr,};
 
 
 };
