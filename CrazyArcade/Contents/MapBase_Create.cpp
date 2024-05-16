5096e3e237a07cc63f58878a7496b545a90cae8c
rla5233 <rla5233@naver.com>
2024-05-16 17:28:01 +0900

    김수원: MapBase 업데이트

--- CrazyArcade/Contents/MapBase_Create.cpp
+++ CrazyArcade/Contents/MapBase_Create.cpp
@@ -10,54 +10,54 @@
 
 void AMapBase::CreateWall(FPoint _Point, std::string_view _ImgName)
 {
-	MapInfo[_Point.Y][_Point.X].Block = GetWorld()->SpawnActor<AWall>("Wall");
-	MapInfo[_Point.Y][_Point.X].Block->SetBlockImg(_ImgName);
-	MapInfo[_Point.Y][_Point.X].Block->GetBody()->SetOrder(Const::MaxOrder - _Point.Y);
+	TileInfo[_Point.Y][_Point.X].Block = GetWorld()->SpawnActor<AWall>("Wall");
+	TileInfo[_Point.Y][_Point.X].Block->SetBlockImg(_ImgName);
+	TileInfo[_Point.Y][_Point.X].Block->GetBody()->SetOrder(Const::MaxOrder - _Point.Y);
 
 	FVector Pos = StartPos;
 	Pos.X += _Point.X * BlockSize;
 	Pos.Y += _Point.Y * BlockSize;
-	MapInfo[_Point.Y][_Point.X].Block->SetActorLocation(Pos);
+	TileInfo[_Point.Y][_Point.X].Block->SetActorLocation(Pos);
 }
 
 void AMapBase::CreateBox(FPoint _Point, std::string_view _ImgName)
 {
-	MapInfo[_Point.Y][_Point.X].Block = GetWorld()->SpawnActor<ABox>("Box");
+	TileInfo[_Point.Y][_Point.X].Block = GetWorld()->SpawnActor<ABox>("Box");
 
-	MapInfo[_Point.Y][_Point.X].Block->GetBody()->CreateAnimation(MapAnim::block_idle, _ImgName, 0.1f, false, 0, 0);
-	MapInfo[_Point.Y][_Point.X].Block->GetBody()->CreateAnimation(MapAnim::block_destroy, _ImgName, 0.1f, false);
-	MapInfo[_Point.Y][_Point.X].Block->GetBody()->ChangeAnimation(MapAnim::block_idle);
-	MapInfo[_Point.Y][_Point.X].Block->GetBody()->SetOrder(Const::MaxOrder - _Point.Y);
+	TileInfo[_Point.Y][_Point.X].Block->GetBody()->CreateAnimation(MapAnim::block_idle, _ImgName, 0.1f, false, 0, 0);
+	TileInfo[_Point.Y][_Point.X].Block->GetBody()->CreateAnimation(MapAnim::block_destroy, _ImgName, 0.1f, false);
+	TileInfo[_Point.Y][_Point.X].Block->GetBody()->ChangeAnimation(MapAnim::block_idle);
+	TileInfo[_Point.Y][_Point.X].Block->GetBody()->SetOrder(Const::MaxOrder - _Point.Y);
 
 	FVector Pos = StartPos;
 	Pos.X += _Point.X * BlockSize;
 	Pos.Y += _Point.Y * BlockSize;
-	MapInfo[_Point.Y][_Point.X].Block->SetActorLocation(Pos);
+	TileInfo[_Point.Y][_Point.X].Block->SetActorLocation(Pos);
 }
 
 void AMapBase::CreateMoveBox(FPoint _Point, std::string_view _ImgName)
 {
-	MapInfo[_Point.Y][_Point.X].Block = GetWorld()->SpawnActor<AMoveBox>("MoveBox");
+	TileInfo[_Point.Y][_Point.X].Block = GetWorld()->SpawnActor<AMoveBox>("MoveBox");
 
-	MapInfo[_Point.Y][_Point.X].Block->GetBody()->CreateAnimation(MapAnim::block_idle, _ImgName, 0.1f, false, 0, 0);
-	MapInfo[_Point.Y][_Point.X].Block->GetBody()->CreateAnimation(MapAnim::block_destroy, _ImgName, 0.1f, false);
-	MapInfo[_Point.Y][_Point.X].Block->GetBody()->ChangeAnimation(MapAnim::block_idle);
-	MapInfo[_Point.Y][_Point.X].Block->GetBody()->SetOrder(Const::MaxOrder - _Point.Y);
+	TileInfo[_Point.Y][_Point.X].Block->GetBody()->CreateAnimation(MapAnim::block_idle, _ImgName, 0.1f, false, 0, 0);
+	TileInfo[_Point.Y][_Point.X].Block->GetBody()->CreateAnimation(MapAnim::block_destroy, _ImgName, 0.1f, false);
+	TileInfo[_Point.Y][_Point.X].Block->GetBody()->ChangeAnimation(MapAnim::block_idle);
+	TileInfo[_Point.Y][_Point.X].Block->GetBody()->SetOrder(Const::MaxOrder - _Point.Y);
 
 	FVector Pos = StartPos;
 	Pos.X += _Point.X * BlockSize;
 	Pos.Y += _Point.Y * BlockSize;
-	MapInfo[_Point.Y][_Point.X].Block->SetActorLocation(Pos);
+	TileInfo[_Point.Y][_Point.X].Block->SetActorLocation(Pos);
 }
 
 void AMapBase::CreateItem(FPoint _Point, EItemType _ItemType)
 {
-	MapInfo[_Point.Y][_Point.X].Item = GetWorld()->SpawnActor<AItemBase>("Item");
+	TileInfo[_Point.Y][_Point.X].Item = GetWorld()->SpawnActor<AItemBase>("Item");
 
 	FVector Pos = StartPos;
 	Pos.X += (_Point.X * BlockSize) + (0.5f * BlockSize);
 	Pos.Y += (_Point.Y * BlockSize) + (0.5f * BlockSize);
-	MapInfo[_Point.Y][_Point.X].Item->SetActorLocation(Pos);
+	TileInfo[_Point.Y][_Point.X].Item->SetActorLocation(Pos);
 
-	MapInfo[_Point.Y][_Point.X].Item->SetItem(_ItemType);
+	TileInfo[_Point.Y][_Point.X].Item->SetItem(_ItemType);
 }
