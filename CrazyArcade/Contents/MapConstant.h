f462211ea8e22442c4455ce9bc936242bb96f207
assortrock <assortrock@DESKTOP-VMF6B3Q>
2024-05-17 14:23:17 +0900

    조선욱 : 포레스트 맵 생성

--- CrazyArcade/Contents/MapConstant.h
+++ CrazyArcade/Contents/MapConstant.h
@@ -5,6 +5,7 @@ namespace MapImgRes
 	// BackGround
 	const std::string_view play_ui_background	= "spr_play_ui_background.png";
 	const std::string_view village_background	= "spr_village1_background.png";
+	const std::string_view Forest_background = "spr_forest_background.png";
 	
 	// Block
 	const std::string_view block_vil_house_0	= "spr_vil_house_0.png";
