2ebf5f65609dbe0d668d822e28ad919f79134423
ChoiSubeen <HellDivers2@gmail.com>
2024-05-21 14:09:39 +0900

    최수빈 : 인트로 레벨 클래스 추가

---
+++ CrazyArcade/Contents/IntroGameMode.cpp
@@ -0,0 +1,25 @@
+#include "PreCompile.h"
+#include "IntroGameMode.h"
+
+AIntroGameMode::AIntroGameMode()
+{
+}
+
+AIntroGameMode::~AIntroGameMode()
+{
+}
+
+void AIntroGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+}
+
+void AIntroGameMode::Tick(float _DeltaTime)
+{
+	Super::Tick(_DeltaTime);
+
+
+}
+
