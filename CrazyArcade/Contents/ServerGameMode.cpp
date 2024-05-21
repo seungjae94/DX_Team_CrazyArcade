d9af590dd0ea201c1b4a3cf4bb1478aeb838512c
usm <AssortRock0@AssortRock>
2024-05-21 16:15:00 +0900

    유성민 : 폭탄

--- CrazyArcade/Contents/ServerGameMode.cpp
+++ CrazyArcade/Contents/ServerGameMode.cpp
@@ -90,7 +90,7 @@ void AServerGameMode::HandlerInit()
 						Bomb = UNetObject::GetNetObject<ABombBase>(_Packet->GetObjectToken());
 						Bomb->SetObjectToken(_Packet->GetObjectToken());
 						Bomb->PushProtocolAsync(_Packet);
-						Send(_Packet);
+						UCrazyArcadeCore::Net->Send(_Packet);
 						//MyBomb->SetObjectToken(_Packet->GetObjectToken());
 
 						//MyBomb->SetActorLocation(_Packet->Pos);
