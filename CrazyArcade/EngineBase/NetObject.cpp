4eaeb5ce1bdc9ca860f43ad877cdd5d06d7bce21
usm <AssortRock0@AssortRock>
2024-05-23 21:11:35 +0900

    유성민 : ServerGameMode, NetObject수정

--- CrazyArcade/EngineBase/NetObject.cpp
+++ CrazyArcade/EngineBase/NetObject.cpp
@@ -2,7 +2,7 @@
 #include "NetObject.h"
 
 // 이 토큰을 부여해줄수 있는건 보통 서버입니다.
-std::atomic<int> UNetObject::CurObjectToken = 0;
+std::atomic<int> UNetObject::CurObjectToken = 1;
 std::map<int, UNetObject*> UNetObject::AllNetObject;
 std::mutex UNetObject::AllNetObjectLock;
 
