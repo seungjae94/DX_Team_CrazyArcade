f348881b7857087ff482657f6f7e139ae5c4b590
usm <AssortRock0@AssortRock>
2024-05-23 20:17:58 +0900

    유성민 : Function Clear 다시

--- CrazyArcade/EngineCore/Level.cpp
+++ CrazyArcade/EngineCore/Level.cpp
@@ -330,8 +330,9 @@ void ULevel::ChangeOrderCollision(std::shared_ptr<UCollision> _Collision, int _P
 
 void ULevel::LevelEnd(ULevel* _NextLevel)
 {
-	Super::LevelStart(_NextLevel);
-	_NextLevel->Functions.clear();
+	Super::LevelEnd(_NextLevel);
+	
+	Functions.clear();
 	for (std::pair<const int, std::list<std::shared_ptr<AActor>>>& TickGroup : Actors)
 	{
 		std::list<std::shared_ptr<AActor>>& GroupActors = TickGroup.second;
@@ -345,7 +346,8 @@ void ULevel::LevelEnd(ULevel* _NextLevel)
 void ULevel::LevelStart(ULevel* _PrevLevel)
 {
 	Super::LevelStart(_PrevLevel);
-	_PrevLevel->Functions.clear();
+	
+	Functions.clear();
 	for (std::pair<const int, std::list<std::shared_ptr<AActor>>>& TickGroup : Actors)
 	{
 		std::list<std::shared_ptr<AActor>>& GroupActors = TickGroup.second;
