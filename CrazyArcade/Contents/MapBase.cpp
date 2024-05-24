38d941d747520607c30424761c4320d917864b74
seungjae94 이승재 <mathlife12345@gmail.com>
2024-05-24 14:17:48 +0900

    이승재: wav 파일 mp3 파일로 변환

--- CrazyArcade/Contents/MapBase.cpp
+++ CrazyArcade/Contents/MapBase.cpp
@@ -283,7 +283,7 @@ std::shared_ptr<ABombBase> AMapBase::SpawnBomb(const FVector& _Pos, APlayer* _Pl
 		NewBomb->SetCurPoint(CurPoint);
 		NewBomb->SetIdle();
 		TileInfo[CurPoint.Y][CurPoint.X].AllBomb.push_back(NewBomb);
-		UEngineSound::SoundPlay("Bomb.wav");
+		UEngineSound::SoundPlay("Bomb.mp3");
 		return NewBomb;
 	}
 	else
