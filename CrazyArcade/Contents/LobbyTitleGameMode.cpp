3eec2dfcfbc77370df7198cc48d6a20d4f0d3a62
jeongwoo 조정우 <choeun1999@gmail.com>
2024-05-24 11:01:53 +0900

    조정우 : StageMap 설정 패킷 생성

--- CrazyArcade/Contents/LobbyTitleGameMode.cpp
+++ CrazyArcade/Contents/LobbyTitleGameMode.cpp
@@ -1766,7 +1766,8 @@ void ALobbyTitleGameMode::GameStart()
 			std::map<int, ConnectUserInfo>& Infos = ConnectionInfo::GetInst().GetUserInfos();
 
 			Packet->Infos = Infos;
-			UCrazyArcadeCore::Net->Send(Packet);
+			UCrazyArcadeCore::NetManager.Send(Packet);
+			//UCrazyArcadeCore::Net->Send(Packet);
 		}
 
 		if (ENetType::Server == UCrazyArcadeCore::NetManager.GetNetType())
@@ -1777,6 +1778,18 @@ void ALobbyTitleGameMode::GameStart()
 			UCrazyArcadeCore::NetManager.Send(Packet);
 			return;
 		}
+
+		// 맵 선택시 보내야할 패킷
+		// 맵 타입 설정
+		//ConnectionInfo::GetInst().SetMapType(StageType);
+		// 패킷 보내기
+		//if (ENetType::Server == UCrazyArcadeCore::NetManager.GetNetType())
+		//{
+		//	std::shared_ptr<UStageUpdatePacket> Packet = std::make_shared<UStageUpdatePacket>();
+		//	Packet->MapType = ConnectionInfo::GetInst().GetCurMapType();
+		//	UCrazyArcadeCore::NetManager.Send(Packet);
+		//}
+
 	}
 }
 
